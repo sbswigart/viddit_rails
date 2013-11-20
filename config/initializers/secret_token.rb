20aedeffe49e17ce580e5f79b3f15013483e2f28
Steven Swigart <sbswigart@gmail.com>
2013-11-20 13:03:09 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Viddit::Application.config.secret_key_base = 'e591f3688f536babb459d170a4af7c70435549da16e0fbcf91fb4512c23ef2acc113a86e64c83d0d148d82c5a935d14aeae28914c33228c5ecb128468365c490'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Viddit::Application.config.session_store :cookie_store, key: '_viddit_session'
