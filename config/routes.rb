d3968f42b88489756ce7cb8c4746f6bbc3c47ae0
Steven Swigart <sbswigart@gmail.com>
2013-11-21 14:10:35 -0800

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Viddit::Application.routes.draw do
-
+  resources :users
   root to: 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
