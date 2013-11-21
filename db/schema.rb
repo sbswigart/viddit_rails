77fa624071a191e81a17e68f422430fcf10d246b
Steven Swigart <sbswigart@gmail.com>
2013-11-21 14:53:50 -0800

    add remember token to users

--- db/schema.rb
+++ db/schema.rb
@@ -23,21 +23,4 @@ ActiveRecord::Schema.define(version: 20131120201743) do
     t.datetime "updated_at"
   end
 
-  create_table "videos", force: true do |t|
-    t.string   "provider_name"
-    t.string   "provider_url"
-    t.string   "author_name"
-    t.string   "title"
-    t.string   "author_url"
-    t.string   "video_url"
-    t.string   "html"
-    t.string   "thumbnail_url"
-    t.integer  "height"
-    t.integer  "width"
-    t.integer  "thumbnail_height"
-    t.integer  "thumbnail_width"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
