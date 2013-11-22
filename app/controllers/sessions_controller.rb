7eb32dcbeed0a4d129b063ff1a760f7c86668a5c
Steven Swigart <sbswigart@gmail.com>
2013-11-21 20:11:52 -0800

    add session create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,17 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to root_url
+		else
+			flash.now[:error] = "Invalid email/password"
+			render 'static_pages/home'
+		end
 	end
 
 	def destroy
-
+		sign_out
+		redirect_to root_url
 	end
 end
