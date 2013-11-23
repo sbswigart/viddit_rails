7645f9ce9f52d6d986f6fad3150f8ef80ab361ad
Steven Swigart <sbswigart@gmail.com>
2013-11-22 20:28:44 -0800

    add masonry js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require masonry.pkgd.min.js
 //= require bootstrap
 //= require jquery
 //= require jquery_ujs
