39bc65562b55a9843ea419e32640fc678d4e0534
Steven Swigart <sbswigart@gmail.com>
2013-11-23 15:12:04 -0800

    put jquery before dependent js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require masonry.pkgd.min.js
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require masonry.pkgd.min.js
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
