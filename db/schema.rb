7884dc086065c63188687be228d6af8f1cce4913
Steven Swigart <sbswigart@gmail.com>
2013-12-16 13:34:17 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122073050) do
+ActiveRecord::Schema.define(version: 20131124033938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 20131122073050) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "video_url"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "user_id",              null: false
+    t.integer  "vote_id",              null: false
+    t.integer  "count",      limit: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
