7ad3ffd92a2f0476b3a537bba2e0a1c5b0586b1c
Steven Swigart <sbswigart@gmail.com>
2013-12-17 13:32:51 -0800

    add justin stats

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131124033938) do
+ActiveRecord::Schema.define(version: 20131216214021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "justin_stats", force: true do |t|
+    t.integer  "viewers_count"
+    t.integer  "streams_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
