1028b8327fdb9886eb6d60450ebe137128c3b1e7
Steven Swigart <sbswigart@gmail.com>
2013-12-23 00:53:35 -0800

    add interval methods

--- app/models/justin_stat.rb
+++ app/models/justin_stat.rb
@@ -1,11 +1,25 @@
 class JustinStat < ActiveRecord::Base
-	def self.chart_data
-    JustinStat.all.map do |stat|
-      {
-        created_at: stat.created_at,
-        viewers_count: stat.viewers_count,
-        streams_count: stat.streams_count,
-      }.to_json
-    end
-  end
+	def self.last_day_in_hours
+		all = JustinStat.where("created_at >= :start_date AND created_at <= :end_date",
+  {start_date: Time.now - 1.day, end_date: Time.now})
+		hours = []
+		all.each_with_index do |stat, index|
+			if index % 60 == 0
+				hours << stat
+			end
+		end
+		hours
+	end
+
+	def self.last_week_in_days
+		all = JustinStat.where("created_at >= :start_date AND created_at <= :end_date",
+  {start_date: Time.now - 7.day, end_date: Time.now})
+		days = []
+		all.each_with_index do |stat, index|
+			if index % 1440 == 0
+				days << stat
+			end
+		end
+		days
+	end
 end
