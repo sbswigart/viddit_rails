7bb84b6352bc15bc99a4aa1846c2648c59b75664
Steven Swigart <sbswigart@gmail.com>
2013-12-23 00:52:38 -0800

    add interval helper

--- app/helpers/justin_stats_helper.rb
+++ app/helpers/justin_stats_helper.rb
@@ -1,2 +1,13 @@
 module JustinStatsHelper
+	def interval(inter)
+		if inter == "minutely"
+	 		@stats = JustinStat.last(60)
+	 	elsif inter == "hourly"
+	 		@stats = JustinStat.last_day_in_hours
+	 	elsif inter == "daily"
+	 		@stats = JustinStat.last_week_in_days
+	 	else
+	 		@stats = JustinStat.last(720)
+	 	end
+	end
 end
